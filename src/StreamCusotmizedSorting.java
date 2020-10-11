cc9fd2cd729a33a7216e5e97b25560748f37cf2e
Avinash Patel <avinash.patel@gaiansolutions.com>
2020-10-11 23:39:20 +0530

    comments added

--- src/StreamCusotmizedSorting.java
+++ src/StreamCusotmizedSorting.java
@@ -6,17 +6,25 @@ public class StreamCusotmizedSorting {
     public static void main(String[] args) {
         CustomizedSorting customizedSorting = new CustomizedSorting();
         customizedSorting.descendingOrderSorting1();
+        customizedSorting.descendingOrderSorting2();
+        customizedSorting.descendingOrderSorting3();
+        customizedSorting.descendingOrderSorting4();
     }
 }
 
 
 class CustomizedSorting {
+    /*
+     * sorted() is natural order sorting i.e ascending order so in-order to sort decreasing order
+     * you need customized sorting for that we need to use Comparator interface which overrides
+     * compare() method.
+     */
     public void descendingOrderSorting1() {
         List<Integer> data = new InputData().listOfIntegerData(); // input data.
         /*
          * Processing starts here.
          */
-        System.out.println(data);
+        System.out.println("Before Sorting : " + data);
         List<Integer> sortedData =
                         data.stream().sorted(new Comparator<Integer>() {
                             @Override
@@ -25,6 +33,62 @@ class CustomizedSorting {
                                 return (obj1 < obj2) ? 1 : (obj1 > obj2) ? -1 : 0;
                             }
                         }).collect(Collectors.toList());
-        System.out.println(sortedData);
+        System.out.println("After Sorting : " + sortedData);
+        data.clear();
+        sortedData.clear();
+    }
+
+    /*
+     * new Comparator<Integer>(){} comparator interface implemented with lambda expression.
+     */
+    public void descendingOrderSorting2() {
+        List<Integer> data = new InputData().listOfIntegerData(); // input data.
+        /*
+         * Processing starts here.
+         */
+        System.out.println("Before Sorting : " + data);
+        List<Integer> sortedData =
+                        data.stream().sorted(
+                                        (obj1, obj2) -> (obj1 < obj2) ? 1 : (obj1 > obj2) ? -1 : 0)
+                                        .collect(Collectors.toList());
+        System.out.println("After Sorting : " + sortedData);
+        data.clear();
+        sortedData.clear();
+    }
+
+    /*
+     * .sorted() internally implements Comparable<?> interface which overrides compareTo method so
+     * by adding - to the obj1 i.e -obj1 compareTo(obj2)
+     */
+    public void descendingOrderSorting3() {
+        List<Integer> data = new InputData().listOfIntegerData(); // input data.
+        /*
+         * Processing starts here.
+         */
+        System.out.println("Before Sorting : " + data);
+        List<Integer> sortedData =
+                        data.stream().sorted((obj1, obj2) -> -obj1.compareTo(obj2))
+                                        .collect(Collectors.toList());
+        System.out.println("After Sorting : " + sortedData);
+        data.clear();
+        sortedData.clear();
+    }
+
+    /*
+     * .sorted() internally implements Comparable<?> interface which overrides compareTo method so
+     * just change the parameters
+     */
+    public void descendingOrderSorting4() {
+        List<Integer> data = new InputData().listOfIntegerData(); // input data.
+        /*
+         * Processing starts here.
+         */
+        System.out.println("Before Sorting : " + data);
+        List<Integer> sortedData =
+                        data.stream().sorted((obj1, obj2) -> obj2.compareTo(obj1))
+                                        .collect(Collectors.toList());
+        System.out.println("After Sorting : " + sortedData);
+        data.clear();
+        sortedData.clear();
     }
 }
