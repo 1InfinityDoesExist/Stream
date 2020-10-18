2c8ee7d61e583a7d9ad688b2bd1b040760c61790
Avinash Patel <avinash.patel@gaiansolutions.com>
2020-10-18 17:15:11 +0530

    stream map

--- src/FunctionJava8.java
+++ src/FunctionJava8.java
@@ -1,3 +1,7 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.function.Function;
 
 public class FunctionJava8 {
@@ -6,8 +10,9 @@ public class FunctionJava8 {
         Executer executer = new Executer();
         executer.fun1();
         executer.fun2();
+        executer.fun3();
+        executer.fun4();
     }
-
 }
 
 
@@ -25,4 +30,32 @@ class Executer {
         Integer finalOutput = func.andThen(func1).apply("Avinash Patel");
         System.out.println(finalOutput);
     }
+
+    public void fun3() {
+        List<String> names = Arrays.asList("Avinsh Patel", "Bebo Shah", "Babu K");
+        Map<String, Integer> mapOutput = convertNamesToMap(names, x -> x.length());
+        System.out.println();
+        System.out.println(mapOutput);
+        System.out.println();
+    }
+
+    private <T, R> Map<T, R> convertNamesToMap(List<T> names, Function<T, R> func) {
+        Map<T, R> result = new HashMap<>();
+        for (T t : names) {
+            result.put(t, func.apply(t));
+        }
+        return result;
+    }
+
+    public void fun4() {
+        List<String> names = Arrays.asList("Avinsh Patel", "Bebo Shah", "Babu K");
+        Map<String, Integer> mapOutput = convertNamesToMap(names, new Executer()::getNameLength);
+        long count = mapOutput.entrySet().stream().filter(x -> x.getValue() > 5).count();
+        System.out.println(count);
+    }
+
+    private int getNameLength(String name) {
+        return name.length();
+    }
+
 }
